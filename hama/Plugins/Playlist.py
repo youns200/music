65af54bccc608a1d5bd6d9b2f3a25748a709fe4f
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-07 12:17:19 -0800

    Update Playlist.py

--- hama/Plugins/Playlist.py
+++ hama/Plugins/Playlist.py
@@ -3,12 +3,12 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from hama import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from hama.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from hama.Decorators.admins import AdminRightsCheck
+from hama.Decorators.checker import checker, checkerCB
+from hama.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
