64b5100de88033d113df091aa5e5c81c9fba16c8
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-07 12:02:54 -0800

    Update Blacklist.py

--- hama/Plugins/Blacklist.py
+++ hama/Plugins/Blacklist.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Yukki import SUDOERS, app
-from Yukki.Database import blacklist_chat, blacklisted_chats, whitelist_chat
+from hama import SUDOERS, app
+from hama.Database import blacklist_chat, blacklisted_chats, whitelist_chat
 
 __MODULE__ = "Blacklist"
 __HELP__ = """
