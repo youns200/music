ee7273c3031b6a8322a70763130e0f7ed0290652
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-07 13:07:12 -0800

    Update others.py

--- hama/Inline/others.py
+++ hama/Inline/others.py
@@ -1,7 +1,7 @@
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, InputMediaPhoto, Message)
 
-from Yukki import db_mem
+from hama import db_mem
 
 
 def others_markup(videoid, user_id):
