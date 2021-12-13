1b25f631f8ccf9e66a1c0b48078f40b259e01e96
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-13 23:21:02 +0300

    Update Downloader.py

--- hama/Core/PyTgCalls/Downloader.py
+++ hama/Core/PyTgCalls/Downloader.py
@@ -38,7 +38,7 @@ def download(videoid: str, mystic, title) -> str:
                 try:
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} داگرتن**\n\n**ناو:** {title[:50]}:\n**قەبارەی فایل:** {size}\n\n**<u>داگرتن:</u>**\n**خێرایی:** {speed}\n**چرکە:** {eta} چرکە\n\n\n{percentage} ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
                         )
                 except Exception as e:
                     pass
@@ -47,21 +47,21 @@ def download(videoid: str, mystic, title) -> str:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME}Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ███▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME}داگرتن**\n\n**ناو:** {title[:50]}:\n**قەبارەی فایڵ:** {size}\n\n**<u>داگرتن:</u>**\n**خێرای:** {speed}\n**کات:** {eta} چرکە\n\n\n{percentage} ███▓▓▓▓▓▓▓▓▓ 100%"
                         )
             if per > 500:
                 if flex[str(bytesx)] == 3:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ██████▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} داگرتن**\n\n**ناو:** {title[:50]}:\n**قەبارەی فایڵ:** {size}\n\n**<u>داگرتن:</u>**\n**خێرای:** {speed}\n**کات:** {eta} چرکە\n\n\n{percentage} ██████▓▓▓▓▓▓ 100%"
                         )
             if per > 800:
                 if flex[str(bytesx)] == 4:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ██████████▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} داگرتن**\n\n**ناو:** {title[:50]}:\n**قەبارەی فایڵ:** {size}\n\n**<u>داگرتن:</u>**\n**خێرای:** {speed}\n**کات:** {eta} چرکە\n\n\n{percentage} ██████████▓▓ 100%"
                         )
         if d["status"] == "finished":
             try:
@@ -70,7 +70,7 @@ def download(videoid: str, mystic, title) -> str:
                 taken = "00:00"
             size = d["_total_bytes_str"]
             mystic.edit(
-                f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n\n100% ████████████100%\n\n**Time Taken:** {taken} Seconds\n\nConverting Audio[FFmpeg Process]"
+                f"**{MUSIC_BOT_NAME} داگرتن**\n\n**ناو:** {title[:50]}:\n\n100% ████████████100%\n\n**کاتی پێویست:** {taken} چرکە\n\nگۆرینی گۆرانی بۆ[FFmpeg Process]"
             )
 
     ydl_optssx = {
