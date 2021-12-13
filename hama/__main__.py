24858c96dc62db9769fbad0aa2737c30dbe340b4
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-13 14:44:32 +0300

    Update __main__.py

--- hama/__main__.py
+++ hama/__main__.py
@@ -233,7 +233,7 @@ async def help_parser(name, keyboard=None):
     )
 
 
-@app.on_callback_query(filters.regex("shikhar"))
+@app.on_callback_query(filters.regex("help_(.*?)"))
 async def shikhar(_, CallbackQuery):
     text, keyboard = await help_parser(CallbackQuery.from_user.mention)
     await CallbackQuery.message.edit(text, reply_markup=keyboard)
