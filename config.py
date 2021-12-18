435754f90d2068fe3e60d58c5cbacf01a30d8c43
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-18 14:40:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,21 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-STRING = getenv("STRING_SESSION", "AgAeQXjYUNrcZ1_gKt-4iyIW9YPMrzUXhNyP2C9qK8iRa7uc8IKR_a09AHcBkrNjOPO5cmyEqhc180s6TvIB4AkXSiujJeF5L1FKJrvMhAMI1esyBf28MWDsh718aVRWcHmfqsSLisGQMNYr37h2TF31PEALD6_dB1L16QXOR8be7gpLlhGppa2cUJ0Lj9dKQNc3psSrV2a5548OLEbf5V-MzzLO0dVOWx5fuPtPwg1oJWJBSv5SStT8_LiObxbLdlF0iy8fKded82virFdijCg7HdjbfD9I9N6SUTqWtP51Rzb3NlmO37PzojZMU8drJkoqlAx6WHrfK_kictou4MQ0Z4b_4QA")
-BOT_TOKEN = getenv("BOT_TOKEN", "2100441086:AAEGGfcEENlB_xd2wXTNq1Elrrm5v1c5Ym8")
-API_ID = int(getenv("API_ID", "6699478"))
-API_HASH = getenv("API_HASH", "28e67d90d53ce22e01d330141824b1da")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "1000"))
+STRING1 = getenv("STRING_SESSION1", "session")
+STRING2 = getenv("STRING_SESSION2", "session")
+STRING3 = getenv("STRING_SESSION3", "session")
+STRING4 = getenv("STRING_SESSION4", "session")
+STRING5 = getenv("STRING_SESSION5", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
 ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hamat:hamat@cluster0.i9wpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "511311707").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "511311707").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001665697505"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Music")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
     SUPPORT_CHANNEL = None
 else:
@@ -25,8 +29,3 @@ else:
 if str(getenv("SUPPORT_GROUP")).strip() == "":
     SUPPORT_GROUP = None
 else:
-    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP"))
-if str(getenv("CC")).strip() == "":
-    CC = None
-else:
-    CC = str(getenv("CC"))
