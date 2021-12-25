619394626eec9b084d29644306f9738266c7c394
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-25 13:05:03 +0300

    Update playlist.py

--- hama/Database/playlist.py
+++ hama/Database/playlist.py
@@ -2,33 +2,14 @@ from typing import Dict, List, Union
 
 from hama import db
 
-playlistdb_lofi = db.playlistlofi
-playlistdb_rock = db.playlistrock
-playlistdb_sad = db.playlistsad
-playlistdb_party = db.playlistparty
-playlistdb_bollywood = db.playlistbollywood
-playlistdb_hollywood = db.playlisthollywood
+
 playlistdb_punjabi = db.playlistpunjabi
-playlistdb_others = db.playlistothers
 
 
 async def _get_playlists(chat_id: int, type: str) -> Dict[str, int]:
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
-        xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
+    if type == "Punjabi":
         xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
+ 
     _notes = await xd.find_one({"chat_id": chat_id})
     if not _notes:
         return {}
@@ -57,22 +38,8 @@ async def save_playlist(chat_id: int, name: str, note: dict, type: str):
     name = name
     _notes = await _get_playlists(chat_id, type)
     _notes[name] = note
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
-        xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
+    if type == "Punjabi":
         xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
     await xd.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
@@ -81,22 +48,8 @@ async def save_playlist(chat_id: int, name: str, note: dict, type: str):
 async def delete_playlist(chat_id: int, name: str, type: str) -> bool:
     notesd = await _get_playlists(chat_id, type)
     name = name
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
-        xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
+    if type == "Punjabi":
         xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
     if name in notesd:
         del notesd[name]
         await xd.update_one(
