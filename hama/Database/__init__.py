81aa61366f7c91c4e3e84a5afd3c41034dc05bf4
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-28 04:16:37 -0800

    Update __init__.py

--- hama/Database/__init__.py
+++ hama/Database/__init__.py
@@ -13,4 +13,4 @@ from .onoff import add_off, add_on, is_on_off
 from .queue import (add_active_chat, get_active_chats, is_active_chat,
                     is_music_playing, music_off, music_on, remove_active_chat)
 from .sudo import add_sudo, get_sudoers, remove_sudo
-from .theme import _get_theme, get_theme, save_theme
+#from .theme import _get_theme, get_theme, save_theme
