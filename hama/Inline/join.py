feddf9498f57472f9de721e63a5628b812572163
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-28 14:22:50 -0800

    Update join.py

--- hama/Inline/join.py
+++ hama/Inline/join.py
@@ -1,4 +1,8 @@
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, InputMediaPhoto, Message)
 
+from hama import db_mem
+from config import CHANNEL, NAMECH
 
 join = InlineKeyboardMarkup(
     [
