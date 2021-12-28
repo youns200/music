2f032252c146b0273467d62bb5bfbbda512238df
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-28 17:29:01 +0300

    Update __init__.py

--- hama/Database/__init__.py
+++ hama/Database/__init__.py
@@ -11,4 +11,4 @@ from .gban import (add_gban_user, get_gbans_count, is_gbanned_user,
 from .queue import (add_active_chat, get_active_chats, is_active_chat,
                     is_music_playing, music_off, music_on, remove_active_chat)
 from .sudo import add_sudo, get_sudoers, remove_sudo
-#from .theme import _get_theme, get_theme, save_theme
+from .blacklistchat import blacklist_chat, blacklisted_chats, whitelist_chat
