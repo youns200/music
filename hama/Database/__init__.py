b5d54b2b5d0e2ac6d776d9945affc7a9965f43b5
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-28 17:23:29 +0300

    Update __init__.py

--- hama/Database/__init__.py
+++ hama/Database/__init__.py
@@ -6,7 +6,8 @@ from .auth import (_get_authusers, add_nonadmin_chat, delete_authuser,
 from .chats import (add_served_chat, get_served_chats, is_served_chat,
                     remove_served_chat)
 from .onoff import add_off, add_on, is_on_off
-
+from .gban import (add_gban_user, get_gbans_count, is_gbanned_user,
+                   remove_gban_user)
 from .queue import (add_active_chat, get_active_chats, is_active_chat,
                     is_music_playing, music_off, music_on, remove_active_chat)
 from .sudo import add_sudo, get_sudoers, remove_sudo
