975e55781976a3135b11a7ce375e53b55b56cea1
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-29 17:43:27 +0300

    Update join.py

--- hama/Plugins/join.py
+++ hama/Plugins/join.py
@@ -6,6 +6,7 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from hama.Inline import join
+
 @app.on_message(filters.command(["play", f"start"]))
 async def handle_force_subscribe(_, message):
     try:
