a9aef616cbc3f1c1a7acf686b6d0390e1ead1467
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2021-12-29 12:42:36 +0300

    Update Play.py

--- hama/Plugins/Play.py
+++ hama/Plugins/Play.py
@@ -33,11 +33,7 @@ loop = asyncio.get_event_loop()
 @PermissionCheck
 @AssistantAdd
 async def play(_, message: Message):
-    await message.delete()
-     if hama.chanel:
-      fsub = await handle_force_subscribe(_, message: Message)
-      if fsub == 400:
-        return
+    await message.delete()     
     if message.chat.id not in db_mem:
         db_mem[message.chat.id] = {}
     if message.sender_chat:
