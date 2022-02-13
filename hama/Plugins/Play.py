5b1e739dbd147261178289b77ac2a2becf061c4f
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2022-02-13 18:02:46 +0300

    Update Play.py

--- hama/Plugins/Play.py
+++ hama/Plugins/Play.py
@@ -38,11 +38,11 @@ async def play(_, message: Message):
     await message.delete()     
     if message.chat.id not in db_mem:
         db_mem[message.chat.id] = {}
-    if chat.id in await blacklisted_chats():
+    if message.chat.id in await blacklisted_chats():
         await message.reply(
             "❗️ ئەم گرووپە بڵۆک کراوە تکایە پەیوەندی بکە بەگروپی پشگیری بۆ چاڵاکردنی من."
         )
-        return await app.leave_chat(chat.id)
+        return await app.leave_chat(message.chat.id)
     if await is_gbanned_user(message.from_user.id):
         await message.reply_text(f"❗️ **تۆ بڵۆک کراویت لەلای من !**")
         return
