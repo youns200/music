1faacf909fc3653554b53d1d70851e8ebff68968
hama-maqsood <86266972+hama-maqsood@users.noreply.github.com>
2022-02-14 20:51:13 +0300

    Update Queues.py

--- hama/Core/PyTgCalls/Queues.py
+++ hama/Core/PyTgCalls/Queues.py
@@ -52,8 +52,8 @@ def clear(chat_id: int):
     raise Empty
 
 def get_queue(chat_id):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
+    if chat_id in Queue:
+        chat_queue = Queue[chat_id]
         return chat_queue
     else:
         return 0
