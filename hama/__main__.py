6f8b98201e40faa0a4f7384ea5b4fd35428952ea
youns200 <100539285+youns200@users.noreply.github.com>
2022-03-25 00:56:57 +0300

    Update __main__.py

--- hama/__main__.py
+++ hama/__main__.py
@@ -214,5 +214,5 @@ async def start_command(_, message):
     )
 
 
-if __name__ == "__main__":
-    loop.run_until_complete(initiate_bot())
+#if __name__ == "__main__":
+    #loop.run_until_complete(initiate_bot())
