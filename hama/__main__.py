4c36e24485190172b99234913b76edb56cf37a45
youns200 <100539285+youns200@users.noreply.github.com>
2022-03-24 18:42:01 +0300

    Update __main__.py

--- hama/__main__.py
+++ hama/__main__.py
@@ -22,7 +22,103 @@ from hama.Utilities.inline import paginate_modules
 
 loop = asyncio.get_event_loop()
 
+console = Console()
+HELPABLE = {}
 
+
+async def initiate_bot():
+    with console.status(
+        "[magenta] Booting up The Amort Music Bot...",
+    ) as status:
+        console.print("┌ [red]Clearing MongoDB cache...")
+        try:
+            chats = await get_active_chats()
+            for chat in chats:
+                chat_id = int(chat["chat_id"])
+                await remove_active_chat(chat_id)
+        except Exception as e:
+            console.print("[red] Error while clearing Mongo DB.")
+        console.print("└ [green]MongoDB Cleared Successfully!\n\n")
+        ____ = await startup_send_new("Importing All Plugins...")
+        status.update(
+            status="[bold blue]Scanning for Plugins", spinner="earth"
+        )
+        await asyncio.sleep(1.7)
+        console.print("Found {} Plugins".format(len(ALL_MODULES)) + "\n")
+        status.update(
+            status="[bold red]Importing Plugins...",
+            spinner="bouncingBall",
+            spinner_style="yellow",
+        )
+        await asyncio.sleep(1.2)
+        for all_module in ALL_MODULES:
+            imported_module = importlib.import_module(
+                "hama.Plugins." + all_module
+            )
+            if (
+                hasattr(imported_module, "__MODULE__")
+                and imported_module.__MODULE__
+            ):
+                imported_module.__MODULE__ = imported_module.__MODULE__
+                if (
+                    hasattr(imported_module, "__HELP__")
+                    and imported_module.__HELP__
+                ):
+                    HELPABLE[
+                        imported_module.__MODULE__.lower()
+                    ] = imported_module
+            console.print(
+                f">> [bold cyan]Successfully imported: [green]{all_module}.py"
+            )
+            await asyncio.sleep(0.2)
+        console.print("")
+        _____ = await startup_edit_last(____, "Finalizing...")
+        status.update(
+            status="[bold blue]Importation Completed!",
+        )
+        await asyncio.sleep(2.4)
+        await startup_delete_last(_____)
+    console.print(
+        "[bold green]Congrats!! Amort Music Bot has started successfully!\n"
+    )
+    try:
+        await app.send_message(
+            LOG_GROUP_ID,
+            "<b>Congrats!! Music Bot has started successfully!</b>",
+        )
+    except Exception as e:
+        print(
+            "Bot has failed to access the log Channel. Make sure that you have added your bot to your log channel and promoted as admin!"
+        )
+        console.print(f"\n[red]Stopping Bot")
+        return
+    a = await app.get_chat_member(LOG_GROUP_ID, BOT_ID)
+    if a.status != "administrator":
+        print("Promote Bot as Admin in Logger Channel")
+        console.print(f"\n[red]Stopping Bot")
+        return
+    try:
+        await userbot.send_message(
+            LOG_GROUP_ID,
+            "<b>Congrats!! Assistant has started successfully!</b>",
+        )
+    except Exception as e:
+        print(
+            "Assistant Account has failed to access the log Channel. Make sure that you have added your bot to your log channel and promoted as admin!"
+        )
+        console.print(f"\n[red]Stopping Bot")
+        return
+    try:
+        await userbot.join_chat("AmortMusic")
+    except:
+        pass
+    console.print(f"\n┌[red] Bot Started as {BOT_NAME}!")
+    console.print(f"├[green] ID :- {BOT_ID}!")
+    console.print(f"├[red] Assistant Started as {ASSNAME}!")
+    console.print(f"└[green] ID :- {ASSID}!")
+    await run()
+    console.print(f"\n[red]Stopping Bot")
+    
 
 @app.on_message(filters.command("start") & filters.private)
 async def start_command(_, message):
@@ -118,5 +214,5 @@ async def start_command(_, message):
     )
 
 
-#if __name__ == "__main__":
-   # loop.run_until_complete(initiate_bot())
+if __name__ == "__main__":
+    loop.run_until_complete(initiate_bot())
