93f3e9145daeecb9b7076fd926313fc5ceb99007
youns200 <100539285+youns200@users.noreply.github.com>
2022-03-26 03:05:46 +0300

    Update hama.py

--- hama/Core/PyTgCalls/hama.py
+++ hama/Core/PyTgCalls/hama.py
@@ -9,8 +9,8 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, KeyboardButton, Message,
                             ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from pytgcalls import PyTgCalls
-from pytgcalls import idle
-from pytgcalls.types import AudioPiped
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream, InputStream
 
 from hama import MUSIC_BOT_NAME, app, db_mem, userbot
 from hama.Core.PyTgCalls import Queues
